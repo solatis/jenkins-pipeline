08fe1ccd157aa561db7fdd99949a901e9d59ce61
Leon Mergen <leon@solatis.com>
2017-10-06 18:20:52 +0200

    Changing build pipeline a bit

--- src/io/solatis/Pipeline.groovy
+++ src/io/solatis/Pipeline.groovy
@@ -91,19 +91,24 @@ def gitEnvVars() {
 
 def containerBuildPub(Map args) {
 
-    println "Running Docker build/publish: ${args.host}/${args.acct}/${args.repo}:${args.tags}"
+    println "Running Docker build/publish: ${args.host}/${args.acct}/${args.repo}"
 
-    docker.withRegistry("https://${args.host}", "${args.auth_id}") {
-
-        // def img = docker.build("${args.acct}/${args.repo}", args.dockerfile)
-        def img = docker.image("${args.acct}/${args.repo}")
-        sh "docker build --build-arg VCS_REF=${env.GIT_SHA} --build-arg BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'` -t ${args.acct}/${args.repo} ${args.dockerfile}"
-        for (int i = 0; i < args.tags.size(); i++) {
-            img.push(args.tags.get(i))
-        }
-
-        return img.id
+    dir(args.dir) {
+      sh "docker build --build-arg VCS_REF=${env.GIT_SHA} --build-arg BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'` -t ${args.acct}/${args.repo} ${args.dockerfile}"
     }
+
+
+    // docker.withRegistry("https://${args.host}", "${args.auth_id}") {
+
+    //     // def img = docker.build("${args.acct}/${args.repo}", args.dockerfile)
+    //     def img = docker.image("${args.acct}/${args.repo}")
+
+    //     for (int i = 0; i < args.tags.size(); i++) {
+    //         img.push(args.tags.get(i))
+    //     }
+
+    //     return img.id
+    // }
 }
 
 def getContainerTags(config, Map tags = [:]) {
