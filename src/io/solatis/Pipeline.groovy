f9b4304a7ebe0b6a61e72dd38c43c9944a2c5f35
Leon Mergen <leon@solatis.com>
2017-10-07 17:28:24 +0200

    Try to figure out what's in the config

--- src/io/solatis/Pipeline.groovy
+++ src/io/solatis/Pipeline.groovy
@@ -95,6 +95,7 @@ def containerBuildPub(Map args) {
 
     dir(args.dir) {
       docker.withRegistry("https://${args.host}", "${args.auth_id}") {
+        sh "cat ~/.dockercfg"
         def img = docker.image("${args.acct}/${args.repo}")
         sh "docker build -t ${args.acct}/${args.repo} ${args.dockerfile}"
         img.push('latest')
