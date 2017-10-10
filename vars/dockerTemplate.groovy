2e6feec8349ac9ddf42bb4ba580cace8dfc0eb4f
Leon Mergen <leon@solatis.com>
2017-10-10 15:10:50 +0200

    MVP

--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -1,9 +1,4 @@
 #!/usr/bin/groovy
 def call(body) {
-  podTemplate(
-    label: label,
-    containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
-    volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
-    body()
-  }
+  body()
 }
